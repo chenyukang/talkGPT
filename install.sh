6e6ff11644bcf73ad3cc712216e868b6a27b0129
yukang <moorekang@gmail.com>
2023-03-08 22:39:31 +0800

    fix install

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-pip install openai
-pip install pyaudio  
-pip install -U openai-whisper
-pip install SpeechRecognition
-
 brew install portaudio
 brew install flac
 brew install ffmpeg
+
+pip install openai
+pip install pyaudio
+pip install -U openai-whisper
+pip install SpeechRecognition
 
