8d628a53fa014faa3f59a32fd20d693872b1900f
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-03-14 09:25:13 +0700

    Change python dependentcies installation with requirements.txt

--- install.sh
+++ install.sh
@@ -2,8 +2,4 @@ brew install portaudio
 brew install flac
 brew install ffmpeg
 
-pip install openai
-pip install pyaudio
-pip install -U openai-whisper
-pip install SpeechRecognition
-
+pip install -r ./requirements.txt
